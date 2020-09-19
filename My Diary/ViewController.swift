aba79a97f6ab4fdc8ab9a0665a2678ec4f34b142
nitinPatil947 <nitin.patil@baseinformation.com>
2020-09-19 14:50:14 +0530

    Initial Commit

---
+++ My Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Diary
+//
+//  Created by Nitin Patil on 19/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
